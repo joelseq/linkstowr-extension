b0462b07ba75007601cfd734ab799330d2d048c3
Joel Sequeira <joelseq96@gmail.com>
2023-07-15 19:06:36 -0400

    Add zip and pre:publish scripts

---
+++ scripts/zipExtension.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import archiver from 'archiver';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Got the following code from ChatGPT
+
+function createZip(directoryPath: string, zipFilePath: string): void {
+  const output = fs.createWriteStream(zipFilePath);
+  const archive = archiver('zip', {
+    zlib: {level: 9}, // Compression level (0-9)
+  });
+
+  output.on('close', () => {
+    console.log(`Zip file created: ${archive.pointer()} total bytes`);
+  });
+
+  archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      console.warn(err);
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on('error', (err) => {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  archive.directory(directoryPath, false); // Include the directory contents but not the directory itself
+
+  archive.finalize();
+}
+
+const directoryPath = path.join(__dirname, '../dist');
+const zipFilePath = path.join(__dirname, '../out/extension.zip');
+
+createZip(directoryPath, zipFilePath);
